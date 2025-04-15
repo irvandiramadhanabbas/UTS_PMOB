1d25c5a6817a254ac547d9a243eb68d11f4f9c83
M-SI <irvandiramadhana3@gmail.com>
2025-04-15 18:43:50 +0700

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,11 +60,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    // Retrofit & Gson
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
-// ViewModel + Coroutine
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation ("androidx.activity:activity-compose:1.7.2")
